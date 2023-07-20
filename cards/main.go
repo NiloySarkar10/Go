0193516da699d1d3a43fc73cd47b842def74c053
Niloy Sarkar <nlysrkr10@gmail.com>
2023-07-21 01:24:43 +0530

    Deck created and save to file in system accomplished.

--- cards/main.go
+++ cards/main.go
@@ -3,11 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	// var card string = "Ace of Spades" ---> Same as the next line
-	card := newCard() // := needs to be used only for the first time declaration (indicate a new variable)
-	fmt.Println(card)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cardsInHand, cardsRemainingInDeck := dealHand(cards, 5)
+	errorFromInHandsCardSave := cardsInHand.saveToFile("in_hand_cards")
+	errorFromInDeckCardSave := cardsRemainingInDeck.saveToFile(("cards_in_deck"))
+	fmt.Println(errorFromInHandsCardSave)
+	fmt.Println(errorFromInDeckCardSave)
 }
